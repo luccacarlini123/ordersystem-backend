dda21fd2e66ef49dc774089ed3bab1d95c302aa9
luccacarlini123 <lucca.carlini18@gmail.com>
2020-05-08 05:56:47 -0300

    Criando projeto e testando REST

---
+++ src/test/java/com/mouzetech/ordersystem/OrdersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mouzetech.ordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
