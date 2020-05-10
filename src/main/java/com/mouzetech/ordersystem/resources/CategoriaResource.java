092fbf9a9a8707ef075ef1fcccb8fa80d01480f5
luccacarlini123 <lucca.carlini18@gmail.com>
2020-05-10 02:28:19 -0300

    Testando primeira classe de dominio - Categoria

--- src/main/java/com/mouzetech/ordersystem/resources/CategoriaResource.java
+++ src/main/java/com/mouzetech/ordersystem/resources/CategoriaResource.java
@@ -1,16 +1,27 @@
 package com.mouzetech.ordersystem.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mouzetech.ordersystem.domain.Categoria;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String listar() {
-		return "REST está funcionando.";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Eletrônico");
+		Categoria cat2 = new Categoria(2, "Cama, mesa e banho");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 	
 }
