8d670f0d99f542191fa5b4c456457f7cf4c5800d
luccacarlini123 <lucca.carlini18@gmail.com>
2020-05-12 05:29:38 -0300

    Estado e Cidade domain

--- src/main/java/com/mouzetech/ordersystem/OrdersystemApplication.java
+++ src/main/java/com/mouzetech/ordersystem/OrdersystemApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.mouzetech.ordersystem.domain.Categoria;
+import com.mouzetech.ordersystem.domain.Cidade;
+import com.mouzetech.ordersystem.domain.Estado;
 import com.mouzetech.ordersystem.domain.Produto;
 import com.mouzetech.ordersystem.repositories.CategoriaRepository;
+import com.mouzetech.ordersystem.repositories.CidadeRepository;
+import com.mouzetech.ordersystem.repositories.EstadoRepository;
 import com.mouzetech.ordersystem.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class OrdersystemApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepo;
+	
+	@Autowired
+	private EstadoRepository estadoRepo;
+	
+	@Autowired
+	private CidadeRepository cidadeRepo;
 	
 
 	public static void main(String[] args) {
@@ -43,8 +53,22 @@ public class OrdersystemApplication implements CommandLineRunner {
 		p2.setCategorias(Arrays.asList(cat1, cat2));
 		p3.setCategorias(Arrays.asList(cat1));
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.setCidade(Arrays.asList(c1));
+		est2.setCidade(Arrays.asList(c2, c3));
+				
 		categoriaRepo.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepo.saveAll(Arrays.asList(p1, p2, p3));
+		estadoRepo.saveAll(Arrays.asList(est1, est2));
+		cidadeRepo.saveAll(Arrays.asList(c1, c2, c3));
+		
+		
 		
 	}
 
--- src/main/java/com/mouzetech/ordersystem/repositories/exceptions/StandardError.java
+++ src/main/java/com/mouzetech/ordersystem/repositories/exceptions/StandardError.java
@@ -34,6 +34,4 @@ public class StandardError implements Serializable{
 		this.timeStamp = timeStamp;
 	}
 	
-	
-	
 }
