1bfd867bc633fa7e9f41a69e7d03635bb601fe4d
luccacarlini123 <lucca.carlini18@gmail.com>
2020-05-12 03:46:37 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/mouzetech/ordersystem/services/CategoriaService.java
+++ src/main/java/com/mouzetech/ordersystem/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.mouzetech.ordersystem.domain.Categoria;
 import com.mouzetech.ordersystem.repositories.CategoriaRepository;
+import com.mouzetech.ordersystem.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,7 @@ public class CategoriaService {
 	
 	public Categoria buscarPorId(Integer id){
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: "+id+". Tipo: "+Categoria.class.getName()));
 	}
 	
 }
