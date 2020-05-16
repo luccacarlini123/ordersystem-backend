0dab19e53dd38527217fd941cadf3b9e3d43a6d2
luccacarlini123 <lucca.carlini18@gmail.com>
2020-05-15 22:03:33 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/mouzetech/ordersystem/resources/CategoriaResource.java
+++ src/main/java/com/mouzetech/ordersystem/resources/CategoriaResource.java
@@ -1,11 +1,16 @@
 package com.mouzetech.ordersystem.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mouzetech.ordersystem.domain.Categoria;
 import com.mouzetech.ordersystem.services.CategoriaService;
@@ -23,4 +28,10 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.inserir(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}	
 }
--- src/main/java/com/mouzetech/ordersystem/services/CategoriaService.java
+++ src/main/java/com/mouzetech/ordersystem/services/CategoriaService.java
@@ -20,4 +20,7 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: "+id+". Tipo: "+Categoria.class.getName()));
 	}
 	
+	public Categoria inserir(Categoria obj) {
+		return repo.save(obj);	
+	}
 }
