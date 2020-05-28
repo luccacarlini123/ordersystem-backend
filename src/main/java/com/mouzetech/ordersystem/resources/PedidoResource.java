c922b9a957c87064737dc269b6c365d5557c2946
luccacarlini123 <lucca.carlini18@gmail.com>
2020-05-28 04:38:47 -0300

    Inserindo Pedido

--- src/main/java/com/mouzetech/ordersystem/resources/PedidoResource.java
+++ src/main/java/com/mouzetech/ordersystem/resources/PedidoResource.java
@@ -1,11 +1,18 @@
 package com.mouzetech.ordersystem.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mouzetech.ordersystem.domain.Pedido;
 import com.mouzetech.ordersystem.services.PedidoService;
@@ -21,6 +28,13 @@ public class PedidoResource {
 	public ResponseEntity<?> findById(@PathVariable Integer id){
 		Pedido obj = service.buscarPorId(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj){
+		obj = service.inserir(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/com/mouzetech/ordersystem/services/PedidoService.java
+++ src/main/java/com/mouzetech/ordersystem/services/PedidoService.java
@@ -1,11 +1,18 @@
 package com.mouzetech.ordersystem.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.mouzetech.ordersystem.domain.ItemPedido;
+import com.mouzetech.ordersystem.domain.PagamentoComBoleto;
 import com.mouzetech.ordersystem.domain.Pedido;
+import com.mouzetech.ordersystem.domain.enums.EstadoPagamento;
+import com.mouzetech.ordersystem.repositories.ItemPedidoRepository;
+import com.mouzetech.ordersystem.repositories.PagamentoRepository;
 import com.mouzetech.ordersystem.repositories.PedidoRepository;
 import com.mouzetech.ordersystem.services.exceptions.ObjectNotFoundException;
 
@@ -15,9 +22,42 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepo;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepo;
+	
 	public Pedido buscarPorId(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Pedido não encontrado. Id: "+id+" Tipo: "+Pedido.class.getName()));
+	}
+
+	@Transactional
+	public Pedido inserir(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepo.save(obj.getPagamento());
+		for(ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.buscarPorId(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepo.saveAll(obj.getItens());
+		return obj;
 	}
 	
 }
