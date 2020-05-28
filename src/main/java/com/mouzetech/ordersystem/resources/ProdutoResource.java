c4965bd5485386e15424c749646b8ca35f43498f
luccacarlini123 <lucca.carlini18@gmail.com>
2020-05-28 01:19:56 -0300

    Busca de Produto por nome e categoria

--- src/main/java/com/mouzetech/ordersystem/resources/ProdutoResource.java
+++ src/main/java/com/mouzetech/ordersystem/resources/ProdutoResource.java
@@ -1,5 +1,48 @@
 package com.mouzetech.ordersystem.resources;
 
-public class ProdutoResource {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mouzetech.ordersystem.domain.Produto;
+import com.mouzetech.ordersystem.dto.ProdutoDTO;
+import com.mouzetech.ordersystem.resources.utils.URL;
+import com.mouzetech.ordersystem.services.ProdutoService;
+
+@RestController
+@RequestMapping(value="/produtos")
+public class ProdutoResource {
+	
+	@Autowired
+	private ProdutoService service;
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<Produto> findById(@PathVariable Integer id){
+		Produto obj = service.buscarPorId(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping
+	public ResponseEntity<Page<ProdutoDTO>> findPage(
+			@RequestParam(value="nome", defaultValue="") String nome,
+			@RequestParam(value="categorias", defaultValue="") String categorias,
+			@RequestParam(value="page", defaultValue="0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage,
+			@RequestParam(value="direction", defaultValue="ASC") String direction,
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy
+			){
+		
+		String nameDecoded = URL.decodeString(nome);
+		List<Integer> ids = URL.decodeIntList(categorias);
+		Page<Produto> list = service.search(nameDecoded, ids, page, linesPerPage, orderBy, direction);
+		Page<ProdutoDTO> listDTO = list.map(obj -> new ProdutoDTO(obj));
+		return ResponseEntity.ok().body(listDTO); 
+	}
 }
--- src/main/java/com/mouzetech/ordersystem/services/ProdutoService.java
+++ src/main/java/com/mouzetech/ordersystem/services/ProdutoService.java
@@ -1,5 +1,42 @@
 package com.mouzetech.ordersystem.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.mouzetech.ordersystem.domain.Categoria;
+import com.mouzetech.ordersystem.domain.Produto;
+import com.mouzetech.ordersystem.repositories.CategoriaRepository;
+import com.mouzetech.ordersystem.repositories.ProdutoRepository;
+import com.mouzetech.ordersystem.services.exceptions.ObjectNotFoundException;
+
+@Service
 public class ProdutoService {
 
+	@Autowired
+	private ProdutoRepository repo;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepo;
+	
+	public List<Produto> buscarTodos(){
+		return repo.findAll();
+	}
+	
+	public Produto buscarPorId(Integer id) {
+		Optional<Produto> produto = repo.findById(id);
+		return produto.orElseThrow(() -> new ObjectNotFoundException("Id não encontrado: "+id+", Tipo: "+Produto.class.getName()));
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepo.findAllById(ids);
+		return repo.search(nome, categorias, pageRequest);	
+	}
+	
 }
