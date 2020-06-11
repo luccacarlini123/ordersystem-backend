88a59311dd0255f444988a3d4b5ee83827d0d76b
luccacarlini123 <lucca.carlini18@gmail.com>
2020-06-11 19:47:42 -0300

    Restrição de conteúdo, cliente recupera só os seus pedidos

--- src/main/java/com/mouzetech/ordersystem/repositories/PedidoRepository.java
+++ src/main/java/com/mouzetech/ordersystem/repositories/PedidoRepository.java
@@ -1,10 +1,18 @@
 package com.mouzetech.ordersystem.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.mouzetech.ordersystem.domain.Cliente;
 import com.mouzetech.ordersystem.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
+	
 }
--- src/main/java/com/mouzetech/ordersystem/services/PedidoService.java
+++ src/main/java/com/mouzetech/ordersystem/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.mouzetech.ordersystem.domain.Cliente;
 import com.mouzetech.ordersystem.domain.ItemPedido;
 import com.mouzetech.ordersystem.domain.PagamentoComBoleto;
 import com.mouzetech.ordersystem.domain.Pedido;
@@ -14,6 +18,8 @@ import com.mouzetech.ordersystem.domain.enums.EstadoPagamento;
 import com.mouzetech.ordersystem.repositories.ItemPedidoRepository;
 import com.mouzetech.ordersystem.repositories.PagamentoRepository;
 import com.mouzetech.ordersystem.repositories.PedidoRepository;
+import com.mouzetech.ordersystem.security.UserSS;
+import com.mouzetech.ordersystem.services.exceptions.AuthorizationException;
 import com.mouzetech.ordersystem.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -67,6 +73,16 @@ public class PedidoService {
 		itemPedidoRepo.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		UserSS user = UserService.authenticated();
+		if(user==null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.buscarPorId(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 	
 }
